0d8ac15d5d4b643b175b9c6d356f78a07eeaa9b5
Chida Toshihirro <toshi.c.1986@gmail.com>
2019-12-30 05:27:44 +0900

    jsx の仕組み

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div><h1>Hello, world!</h1></div>
+      <h1>Hello, world!</h1>
     );
   }
 }
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello, world"
+//     );
+//   }
+// }
 
 export default App;
