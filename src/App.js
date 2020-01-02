56a63f571d30ec52a11a85aa53c40db16356dee2
Chida Toshihirro <toshi.c.1986@gmail.com>
2020-01-03 07:05:06 +0900

    クラスコンポーネント 関数コンポーネント 作成

--- src/App.js
+++ src/App.js
@@ -35,14 +35,34 @@ import React, { Component } from 'react';
 //   }
 // }
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onClick={() => {console.log("Hi!")}} />
-      </React.Fragment>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onClick={() => {console.log("Hi!")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+// const App = () => {
+//   return <div>Hi!</div>
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
+
+const Cat = () => {
+  return <div>Meow!</div>
+}
+
 export default App;
