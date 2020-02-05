c04fd17980484b437500059db24ec9e3fbddb0fe
Chida Toshihirro <toshi.c.1986@gmail.com>
2020-02-06 03:47:45 +0900

    router

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-
+import { Link } from 'react-router-dom';
 import { readEvents } from '../actions'
 
 class EventsIndex extends Component {
@@ -34,6 +34,8 @@ class EventsIndex extends Component {
             {this.renderEvents()}
           </tbody>
         </table>
+
+        <Link to="/events/new">New Event</Link>
       </React.Fragment>
     );
 
