d32522f810931fe6fb364eb1db04f9785620c102
Chida Toshihirro <toshi.c.1986@gmail.com>
2020-02-06 19:50:35 +0900

    impliment eventsShow

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,11 +1,20 @@
 import _ from 'lodash'
 import {
+    CREATE_EVENT,
     READ_EVENTS,
-    DELETE_EVENT
+    READ_EVENT,
+    DELETE_EVENT,
+    UPDATE_EVENT
 } from '../actions';
 
 export default (events = {}, action) => {
     switch (action.type) {
+        case READ_EVENT:
+        case CREATE_EVENT:
+        case UPDATE_EVENT:
+            // {id: 5, title: "Let's have an event 5!", body: "This is the body for event 5."}
+            const data = action.response.data;
+            return { ...events, [data.id]:data }
         case READ_EVENTS:
             return _.mapKeys(action.response.data, 'id');
         case DELETE_EVENT:
